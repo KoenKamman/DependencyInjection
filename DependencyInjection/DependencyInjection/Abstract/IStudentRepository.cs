d5e8337fa87c9a06b9229c9e71d71d94e60d1a19
Koen Kamman <kammankoen@gmail.com>
2017-09-03 15:59:28 +0200

    DependencyInjectionDemo

---
+++ DependencyInjection/DependencyInjection/Abstract/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DependencyInjection.Models;
+
+namespace DependencyInjection.Abstract
+{
+    public interface IStudentRepository
+    {
+	    bool AddStudent(Student student);
+	    bool DeleteStudent(Student student);
+	    IEnumerable<Student> GetStudents();
+	    bool EditStudent(Student student);
+    }
+}
--- DependencyInjection/DependencyInjection/Program.cs
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using DependencyInjection.Concrete;
 
 namespace DependencyInjection
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var admin = new StudentAdministration(new TestStudentRepository());
+			admin.AddFiveStudents();
+			admin.PrintAllStudents();
+
+			Console.ReadLine();
+		}
+	}
 }
